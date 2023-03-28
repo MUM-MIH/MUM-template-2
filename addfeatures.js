d5f1cf872d028a507326bb4b1683909f0a132c69
Caryn-Monash <91648753+Caryn-Monash@users.noreply.github.com>
2023-03-28 14:18:58 +0800

    Update addfeatures.js

--- addfeatures.js
+++ addfeatures.js
@@ -1,5 +1,9 @@
 // directs the function to work once the page has loaded.
 window.addEventListener("load", function () {
+ 
+  //add essfs advertisement
+    $("#region-main").before('<a href="https://essfs.ureview.org/" target="_blank"><img src="https://mum-mih.github.io/MUM-template-2/ESSFS_moodle.png" alt="Take the Early Semester Student Feedback Survey and stand a chance to win prizes" style="width:1750px;margin-bottom:15px"></a>');
+
   // change all links that have #section- in them which goes to the all sections page, to &section= links that go to the single section instead
   $("nav a, .bookexit").each(function () {
     this.href = this.href.replace("#section-", "&section=");
@@ -62,12 +66,7 @@ window.addEventListener("load", function () {
     }
   }
   
-  //add essfs advertisement
-if (SchoolSpecific.match(/Important Policy/)) {
-    $("#region-main").before('<a href="https://essfs.ureview.org/"><img src="https://mum-mih.github.io/MUM-template-2/ESSFS_moodle.png" alt="Take the Early Semester Student Feedback Survey and stand a chance to win prizes" style="width:1750px;margin-bottom:15px"></a>');
-} else {
-    console.log("essfs banner fail to load");
-}
+  
   
   //check if we are on the important policy section and if that section is section20
   if (
