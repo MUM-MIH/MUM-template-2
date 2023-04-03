17b5f7b030fe1552af09610d1d88cfdf7768a67c
Caryn-Monash <91648753+Caryn-Monash@users.noreply.github.com>
2023-04-03 15:19:08 +0800

    remove essfs ad

--- addfeatures.js
+++ addfeatures.js
@@ -1,9 +1,7 @@
 // directs the function to work once the page has loaded.
 window.addEventListener("load", function () {
  
-  //add essfs advertisement
-    $("#region-main").before('<a href="https://essfs.ureview.org/" target="_blank"><img src="https://mum-mih.github.io/MUM-template-2/ESSFS_moodle.png" alt="Take the Early Semester Student Feedback Survey and stand a chance to win prizes" style="width:1750px;margin-bottom:15px"></a>');
-
+ 
   // change all links that have #section- in them which goes to the all sections page, to &section= links that go to the single section instead
   $("nav a, .bookexit").each(function () {
     this.href = this.href.replace("#section-", "&section=");
